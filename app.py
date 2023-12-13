eae26ab5fa1f5b6b30b44cd6ff3baf8172fc7582
imene.maouche <imene.maouche@rtit-consulting.com>
2023-12-13 20:45:45 +0100

    Création des bdd contact groupe et interface GUI

--- app.py
+++ app.py
@@ -14,60 +14,57 @@ def add_contact(conn, cursor, nom, prenom, email=None, telephone=None):
     ''', (nom, prenom, email, telephone))
     conn.commit()
 
-class CarnetAdressesApp:
+def search_contacts(cursor, recherche):
+    cursor.execute('''
+        SELECT * FROM contacts
+        WHERE nom LIKE ? OR prenom LIKE ? OR email LIKE ? OR telephone LIKE ?
+    ''', (f'%{recherche}%', f'%{recherche}%', f'%{recherche}%', f'%{recherche}%'))
+    return cursor.fetchall()
+
+def delete_contact(conn, cursor, contact_id):
+    cursor.execute('''
+        DELETE FROM contacts
+        WHERE id = ?
+    ''', (contact_id,))
+    conn.commit()
+
+class Carnet_Gestion_Adresses:
     def __init__(self, root):
         self.root = root
         self.root.title("Carnet d'adresses")
 
-        # Création des champs de saisie
-        self.label_nom = ttk.Label(root, text="Nom:")
-        self.entry_nom = ttk.Entry(root)
+        # ... (champs de saisie, bouton Ajouter, champs de recherche)
 
-        self.label_prenom = ttk.Label(root, text="Prénom:")
-        self.entry_prenom = ttk.Entry(root)
+        # Champs de suppression
+        self.label_suppression = ttk.Label(root, text="ID du contact à supprimer:")
+        self.entry_suppression = ttk.Entry(root)
+        self.button_supprimer = ttk.Button(root, text="Supprimer", command=self.supprimer_contact)
 
-        self.label_email = ttk.Label(root, text="E-mail:")
-        self.entry_email = ttk.Entry(root)
+        # Positionnement des éléments de suppression
+        self.label_suppression.grid(row=7, column=0, padx=10, pady=5, sticky="w")
+        self.entry_suppression.grid(row=7, column=1, padx=10, pady=5)
+        self.button_supprimer.grid(row=7, column=2, pady=5)
 
-        self.label_telephone = ttk.Label(root, text="Téléphone:")
-        self.entry_telephone = ttk.Entry(root)
+    def supprimer_contact(self):
+        contact_id = self.entry_suppression.get()
 
-        # Bouton d'ajout
-        self.button_ajouter = ttk.Button(root, text="Ajouter", command=self.ajouter_contact)
+        # Vérifier si l'ID est un entier
+        try:
+            contact_id = int(contact_id)
+        except ValueError:
+            # Afficher un message d'erreur si l'ID n'est pas un entier
+            tk.messagebox.showerror("Erreur", "Veuillez entrer un ID valide.")
+            return
 
-        # Positionnement des éléments
-        self.label_nom.grid(row=0, column=0, padx=10, pady=5, sticky="w")
-        self.entry_nom.grid(row=0, column=1, padx=10, pady=5)
-
-        self.label_prenom.grid(row=1, column=0, padx=10, pady=5, sticky="w")
-        self.entry_prenom.grid(row=1, column=1, padx=10, pady=5)
-
-        self.label_email.grid(row=2, column=0, padx=10, pady=5, sticky="w")
-        self.entry_email.grid(row=2, column=1, padx=10, pady=5)
-
-        self.label_telephone.grid(row=3, column=0, padx=10, pady=5, sticky="w")
-        self.entry_telephone.grid(row=3, column=1, padx=10, pady=5)
-
-        self.button_ajouter.grid(row=4, column=0, columnspan=2, pady=10)
-
-    def ajouter_contact(self):
-        nom = self.entry_nom.get()
-        prenom = self.entry_prenom.get()
-        email = self.entry_email.get()
-        telephone = self.entry_telephone.get()
-
-        # Ajouter le contact à la base de données
+        # Supprimer le contact de la base de données
         conn, cursor = connect_database()
-        add_contact(conn, cursor, nom, prenom, email, telephone)
+        delete_contact(conn, cursor, contact_id)
         conn.close()
 
-        # Effacer les champs de saisie après l'ajout
-        self.entry_nom.delete(0, "end")
-        self.entry_prenom.delete(0, "end")
-        self.entry_email.delete(0, "end")
-        self.entry_telephone.delete(0, "end")
+        # Effacer les champs de saisie après la suppression
+        self.entry_suppression.delete(0, "end")
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = CarnetAdressesApp(root)
+    app = Carnet_Gestion_Adresses(root)
     root.mainloop()
